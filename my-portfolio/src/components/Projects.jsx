7c1d71082de853ab0976778947d42236c578912b
Ton Nom <maissa.elbhi@example.com>
2024-12-16 14:26:38 +0300

    aj numerique

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Projects.css';
 
 function Projects() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalImage, setModalImage] = useState('');
 
   const featuredProjects = [
     { title: 'Projet 1', description: 'Description du projet 1', link: '#' },
     { title: 'Projet 2', description: 'Description du projet 2', link: '#' },
     { title: 'Projet 3', description: 'Description du projet 3', link: '#' },
-
     { title: 'Projet 4', description: 'Description du projet 4', link: '#' },
     { title: 'Projet 5', description: 'Description du projet 5', link: '#' },
     { title: 'Projet 6', description: 'Description du projet 6', link: '#' },
@@ -15,35 +16,46 @@ function Projects() {
 
   const digitalProjects = [
     { title: 'Pokémon', description: 'Carte postale Pokémon', link: './src/img/pokemon-card.png' },
-    { title: 'Projet Numérique 2', description: 'Description du projet numérique 2', link: '#' },
-    { title: 'Projet Numérique 3', description: 'Description du projet numérique 3', link: '#' },
-
-    // { title: 'Projet Numérique 4', description: 'Description du projet numérique 4', link: '#' },
-    // { title: 'Projet Numérique 5', description: 'Description du projet numérique 5', link: '#' },
-    // { title: 'Projet Numérique 6', description: 'Description du projet numérique 6', link: '#' },
+    // { title: 'Projet Numérique 2', description: 'Description du projet numérique 2', link: '#' },
+    // { title: 'Projet Numérique 3', description: 'Description du projet numérique 3', link: '#' },
   ];
 
   const artisticProjects = [
     { title: 'Projet Artistique 1', description: 'Description du projet artistique 1', link: '#' },
     { title: 'Projet Artistique 2', description: 'Description du projet artistique 2', link: '#' },
     { title: 'Projet Artistique 3', description: 'Description du projet artistique 3', link: '#' },
-
     { title: 'Projet Artistique 4', description: 'Description du projet artistique 4', link: '#' },
     { title: 'Projet Artistique 5', description: 'Description du projet artistique 5', link: '#' },
     { title: 'Projet Artistique 6', description: 'Description du projet artistique 6', link: '#' },
   ];
 
+  const openModal = (image) => {
+    setModalImage(image);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setModalImage('');
+  };
+
+  const renderDigitalProjects = (projects) => {
+  return projects.map((project, index) => (
+    <div key={index} className="project-card">
+      <h3>{project.title}</h3>
+      <p>{project.description}</p>
+      <button className="digital-project-btn" onClick={() => openModal(project.link)}>Voir plus</button>
+    </div>
+  ));
+};
 
 
-  const renderProjects = (projects) => {
+  const renderOtherProjects = (projects) => {
     return projects.map((project, index) => (
       <div key={index} className="project-card">
-        {/* <div className="planet"></div> */}
         <h3>{project.title}</h3>
         <p>{project.description}</p>
-        <a href={project.link} target="_blank" rel="noopener noreferrer">
-          Voir plus
-        </a>
+        <a href={project.link} target="_blank" rel="noopener noreferrer">Voir plus</a>
       </div>
     ));
   };
@@ -53,19 +65,28 @@ function Projects() {
       <div className="projects-container">
         <h2>Mes Projets vedettes</h2>
         <div className="projects-grid">
-          {renderProjects(featuredProjects)}
+          {renderOtherProjects(featuredProjects)}
         </div>
 
         <h2>Projet Numérique</h2>
         <div className="projects-carousel">
-          {renderProjects(digitalProjects)}
+          {renderDigitalProjects(digitalProjects)}
         </div>
 
         <h2>Projet Artistique</h2>
         <div className="projects-carousel">
-          {renderProjects(artisticProjects)}
+          {renderOtherProjects(artisticProjects)}
         </div>
       </div>
+
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>&times;</span>
+            <img src={modalImage} alt="Project" className="modal-image" />
+          </div>
+        </div>
+      )}
     </section>
   );
 }
