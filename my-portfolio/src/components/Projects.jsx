6b09830fa3bfe079f94570bf4c832bb0d28e8637
Ton Nom <maissaa.elbhi@gmail.com>
2025-01-17 19:46:33 +0100

    proj modif

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -12,7 +12,7 @@ function Projects() {
 
   const featuredProjects = [
     // { title: 'Projet 1', description: 'Description du projet 1', link: 'https://www.google.com/?client=safari' },
-    { title: 'MangaKo', description: 'Biblio Manga', link: 'https://projet-react-eosin.vercel.app/' },
+    { title: 'MangaKo', description: 'Biblio Manga-en cours', link: 'https://projet-react-eosin.vercel.app/' },
     { title: 'Pokedex', description: 'Liste des Pokémons', link: 'https://formation-2024-2025.vercel.app' },
     // { title: 'Projet 4', description: 'Description du projet 4', link: '#' },
     // { title: 'Projet 5', description: 'Description du projet 5', link: '#' },
