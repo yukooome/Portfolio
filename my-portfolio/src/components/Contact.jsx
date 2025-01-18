1d420b143158b1e8ca5ae9826800c8e4ae53744c
Ton Nom <maissaa.elbhi@gmail.com>
2025-01-18 21:04:55 +0100

    mod

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -15,7 +15,7 @@ function Contact() {
       
       {/* Copyright */}
       <footer className="contact-footer">
-        <p>&copy; {new Date().getFullYear()} Maissaa ElBachiri. Tous droits réservés.</p>
+        <p>&copy; {new Date().getFullYear()} Maissaa El B. Tous droits réservés.</p>
       </footer>
     </section>
   );
