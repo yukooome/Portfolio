8d9eaf43d283a014a1cdb456c8c75552ad03dfee
Ton Nom <maissaa.elbhi@gmail.com>
2025-01-19 13:26:13 +0100

    supp home

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -15,7 +15,8 @@ function Home() {
 
   const competences = [
     'VSCode','HTML', 'CSS', 'JavaScript', 'React', 'WordPress','Node.js', 'MongoDB Compass','Bootstrap', 'Tailwind CSS', 'SASS/SCSS', 'Figma','Git','GitHub', 'Animate', 
-    'Photoshop', 'Illustrator', 'Procreate', 'Dessin','Peinture', 'Teams', 'One Drive'
+    'Photoshop', 'Illustrator', 'Procreate', 'Dessin','Peinture'
+    // , 'Teams', 'One Drive'
   ];
 
   // Fonction pour faire défiler vers la gauche
