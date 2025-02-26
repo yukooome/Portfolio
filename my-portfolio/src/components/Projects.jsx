b1764545815d0feb3afbb9a883b4b4927f3d8872
Ton Nom <maissaa.elbhi@gmail.com>
2025-02-26 09:36:17 +0100

    chan name

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -19,7 +19,7 @@ function Projects() {
 
 
   const featuredProjects = [
-    { title: 'Artify', description: 'React-Wp - en cours', link: 'https://artify-art-sigma.vercel.app/' , gitLink : 'https://github.com/yukooome/React/tree/main/artify'},
+    { title: 'Artify', description: 'React - en cours', link: 'https://artify-art-sigma.vercel.app/' , gitLink : 'https://github.com/yukooome/React/tree/main/artify'},
     { title: 'Artcadabra', description: 'React - en cours', link: 'https://artcadabra.vercel.app/', gitLink : 'https://github.com/yukooome/React/tree/main/artcadabra' },
     { title: 'MangaKo', description: 'React - en cours', link: 'https://projet-react-eosin.vercel.app/', gitLink : 'https://github.com/yukooome/Projet-React' },
     { title: 'Pokedex', description: 'React', link: 'https://pokemons-pearl-ten.vercel.app', gitLink : 'https://github.com/yukooome/React/tree/main/Pokemons' },
